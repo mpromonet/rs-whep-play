6f53ab803c553b466a167bc825168e72d3bf18d8
Michel Promonet <michel.promonet@free.fr>
2023-05-03 19:53:30 +0200

    first commit

---
+++ src/main.rs
@@ -0,0 +1,172 @@
+use anyhow::Result;
+use serde_json::json;
+use std::env;
+use std::sync::Arc;
+use tokio::sync::Notify;
+use webrtc::api::interceptor_registry::register_default_interceptors;
+use webrtc::api::media_engine::{MediaEngine, MIME_TYPE_H264};
+use webrtc::api::APIBuilder;
+use webrtc::ice_transport::ice_connection_state::RTCIceConnectionState;
+use webrtc::ice_transport::ice_server::RTCIceServer;
+use webrtc::interceptor::registry::Registry;
+use webrtc::peer_connection::configuration::RTCConfiguration;
+use webrtc::peer_connection::sdp::session_description::RTCSessionDescription;
+use webrtc::rtp_transceiver::rtp_codec::{RTCRtpCodecCapability, RTCRtpCodecParameters, RTPCodecType};
+use webrtc::track::track_remote::TrackRemote;
+
+async fn handle_data(track: Arc<TrackRemote>, notify: Arc<Notify>) -> Result<()> {
+    loop {
+        tokio::select! {
+            result = track.read_rtp() => {
+                if let Ok((rtp_packet, _)) = result {
+                    println!("{rtp_packet}");
+                }else{
+                    println!("read_rtp error");
+                    return Ok(());
+                }
+            }
+            _ = notify.notified() => {
+                println!("notified");
+                return Ok(());
+            }
+        }
+    }
+}
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    let mut url =
+        "http://localhost:8000/api/whep?url=Zeeland&options=rtptransport%3dtcp%26timeout%3d60";
+    let args: Vec<String> = env::args().collect();
+    if args.len() > 1 {
+        url = &args[1];
+    }
+
+    // Create a MediaEngine object to configure the supported codec
+    let mut m = MediaEngine::default();
+
+    // Setup the codecs you want to use.
+    // We'll use a H264 and Opus but you can also define your own
+    m.register_codec(
+        RTCRtpCodecParameters {
+            capability: RTCRtpCodecCapability {
+                mime_type: MIME_TYPE_H264.to_owned(),
+                clock_rate: 90000,
+                channels: 0,
+                sdp_fmtp_line: "".to_owned(),
+                rtcp_feedback: vec![],
+            },
+            payload_type: 102,
+            ..Default::default()
+        },
+        RTPCodecType::Video,
+    )?;
+
+    // Create a InterceptorRegistry. This is the user configurable RTP/RTCP Pipeline.
+    // This provides NACKs, RTCP Reports and other features. If you use `webrtc.NewPeerConnection`
+    // this is enabled by default. If you are manually managing You MUST create a InterceptorRegistry
+    // for each PeerConnection.
+    let mut registry = Registry::new();
+
+    // Use the default set of Interceptors
+    registry = register_default_interceptors(registry, &mut m)?;
+
+    // Create the API object with the MediaEngine
+    let api = APIBuilder::new()
+        .with_media_engine(m)
+        .with_interceptor_registry(registry)
+        .build();
+
+    // Prepare the configuration
+    let config = RTCConfiguration {
+        ice_servers: vec![RTCIceServer {
+            urls: vec!["stun:stun.l.google.com:19302".to_owned()],
+            ..Default::default()
+        }],
+        ..Default::default()
+    };
+
+    // Create a new RTCPeerConnection
+    let peer_connection = Arc::new(api.new_peer_connection(config).await?);
+
+    // Allow us to receive 1 video track
+    peer_connection
+        .add_transceiver_from_kind(RTPCodecType::Video, None)
+        .await?;
+
+    let notify_tx = Arc::new(Notify::new());
+    let notify_rx = notify_tx.clone();
+
+    // Set a handler for when a new remote track starts
+    peer_connection.on_track(Box::new(move |track, _, _| {
+        let notify_rx2 = Arc::clone(&notify_rx);
+        Box::pin(async move {
+            let codec = track.codec();
+            let mime_type = codec.capability.mime_type.to_lowercase();
+            if mime_type == MIME_TYPE_H264.to_lowercase() {
+                println!("Got h264 track, receiving data");
+                tokio::spawn(async move {
+                    let _ = handle_data(track, notify_rx2).await;
+                });
+            }
+        })
+    }));
+
+    let (done_tx, mut done_rx) = tokio::sync::mpsc::channel::<()>(1);
+
+    // Set the handler for ICE connection state
+    // This will notify you when the peer has connected/disconnected
+    peer_connection.on_ice_connection_state_change(Box::new(
+        move |connection_state: RTCIceConnectionState| {
+            println!("Connection State has changed {connection_state}");
+
+            if connection_state == RTCIceConnectionState::Connected {
+                println!("Ctrl+C the remote client to stop the demo");
+            } else if connection_state == RTCIceConnectionState::Failed {
+                notify_tx.notify_waiters();
+
+                println!("Done writing media files");
+
+                let _ = done_tx.try_send(());
+            }
+            Box::pin(async {})
+        },
+    ));
+
+    // Create offer
+    let offer = peer_connection.create_offer(None).await?;
+    let offer_str = serde_json::to_string(&offer.sdp)?;
+
+    // Set the remote SessionDescription
+    peer_connection.set_local_description(offer).await?;
+
+    // Create channel that is blocked until ICE Gathering is complete
+    let mut gather_complete = peer_connection.gathering_complete_promise().await;
+    let _ = gather_complete.recv().await;
+
+    // WHEP call
+    println!("Offer:{offer_str}");
+
+    let client = reqwest::Client::new();
+    let response = client.post(url).body(offer_str).send().await?;
+    let answer_str = response.text().await?;
+    println!("Answer:{answer_str}");
+
+    let desc = json!({ "type": "answer", "sdp": answer_str }).to_string();
+    let answer = serde_json::from_str::<RTCSessionDescription>(&desc)?;
+    peer_connection.set_remote_description(answer).await?;
+
+    println!("Press ctrl-c to stop");
+    tokio::select! {
+        _ = done_rx.recv() => {
+            println!("received done signal!");
+        }
+        _ = tokio::signal::ctrl_c() => {
+            println!();
+        }
+    };
+
+    peer_connection.close().await?;
+
+    Ok(())
+}
